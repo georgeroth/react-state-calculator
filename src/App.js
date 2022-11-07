e88681df51d6d4fcbe8b8528c2dc1af105e1ab05
wagnerAJS221 <wagneralex221@gmai.com>
2022-11-07 10:53:59 +0000

    r bug fixed and leading 0 fixed. Margins corrected

--- src/App.js
+++ src/App.js
@@ -1,54 +1,56 @@
-import "./App.css";
-import { useState } from "react";
+import './App.css'
+import { useState } from 'react'
 
 function App() {
-  const [firstNumber, setFirstNumber] = useState("0");
-  const [secondNumber, setSecondNumber] = useState("0");
-  const [totalNumber, setTotalNumber] = useState(0);
-  const [operation, setOperation] = useState("+");
-  const [storedNumber, setStoredNumber] = useState(totalNumber);
+  const [firstNumber, setFirstNumber] = useState('0')
+  const [secondNumber, setSecondNumber] = useState('0')
+  const [totalNumber, setTotalNumber] = useState(0)
+  const [operation, setOperation] = useState('+')
+  const [storedNumber, setStoredNumber] = useState(totalNumber)
 
   function displayNumber(number) {
-    if (firstNumber[0] === "0" && number === "c") {
-      setFirstNumber("0");
-    } else if (firstNumber[0] === "0") {
-      setFirstNumber(number);
-    } else if (number === "c") {
-      setFirstNumber("0");
-    } else if (number === "r") {
-      setFirstNumber(storedNumber);
+    if (firstNumber[0] === '0' && number === 'c') {
+      setFirstNumber('0')
+    } else if (number === 'r') {
+      setFirstNumber(storedNumber)
+    } else if (firstNumber[0] === '0' || firstNumber[0] === undefined) {
+      console.log(firstNumber[0])
+      setFirstNumber(number)
+    } else if (number === 'c') {
+      setFirstNumber('0')
     } else {
-      setFirstNumber(firstNumber + number);
+      console.log(firstNumber[0])
+      setFirstNumber(firstNumber + number)
     }
   }
 
   function displaySecondNumber(number) {
-    if (secondNumber[0] === "0" && number === "c") {
-      setSecondNumber("0");
-    } else if (secondNumber[0] === "0") {
-      setSecondNumber(number);
-    } else if (number === "c") {
-      setSecondNumber("0");
-    } else if (number === "r") {
-      setSecondNumber(storedNumber);
+    if (secondNumber[0] === '0' && number === 'c') {
+      setSecondNumber('0')
+    } else if (number === 'r') {
+      setSecondNumber(storedNumber)
+    } else if (secondNumber[0] === '0' || secondNumber[0] === undefined) {
+      setSecondNumber(number)
+    } else if (number === 'c') {
+      setSecondNumber('0')
     } else {
-      setSecondNumber(secondNumber + number);
+      setSecondNumber(secondNumber + number)
     }
   }
 
   function displayOperation(character) {
-    setOperation(character);
+    setOperation(character)
   }
 
   function smartMaths() {
-    if (operation === "+") {
-      setTotalNumber(+firstNumber + +secondNumber);
-    } else if (operation === "-") {
-      setTotalNumber(+firstNumber - +secondNumber);
-    } else if (operation === "*") {
-      setTotalNumber(+firstNumber * +secondNumber);
-    } else if (operation === "÷") {
-      setTotalNumber(+firstNumber / +secondNumber);
+    if (operation === '+') {
+      setTotalNumber(+firstNumber + +secondNumber)
+    } else if (operation === '-') {
+      setTotalNumber(+firstNumber - +secondNumber)
+    } else if (operation === '*') {
+      setTotalNumber(+firstNumber * +secondNumber)
+    } else if (operation === '÷') {
+      setTotalNumber(+firstNumber / +secondNumber)
     }
   }
 
@@ -57,57 +59,62 @@ function App() {
       <div className="panel">
         <p>{firstNumber}</p>
         <div className="numbers">
-          <button onClick={() => displayNumber("1")}>1</button>
-          <button onClick={() => displayNumber("2")}>2</button>
-          <button onClick={() => displayNumber("3")}>3</button>
-          <button onClick={() => displayNumber("4")}>4</button>
-          <button onClick={() => displayNumber("5")}>5</button>
-          <button onClick={() => displayNumber("6")}>6</button>
-          <button onClick={() => displayNumber("7")}>7</button>
-          <button onClick={() => displayNumber("8")}>8</button>
-          <button onClick={() => displayNumber("9")}>9</button>
-          <button onClick={() => displayNumber("0")}>0</button>
-          <button onClick={() => displayNumber("c")}>Clear</button>
-          <button onClick={() => displayNumber("r")}>Recall</button>
+          <button onClick={() => displayNumber('1')}>1</button>
+          <button onClick={() => displayNumber('2')}>2</button>
+          <button onClick={() => displayNumber('3')}>3</button>
+          <button onClick={() => displayNumber('4')}>4</button>
+          <button onClick={() => displayNumber('5')}>5</button>
+          <button onClick={() => displayNumber('6')}>6</button>
+          <button onClick={() => displayNumber('7')}>7</button>
+          <button onClick={() => displayNumber('8')}>8</button>
+          <button onClick={() => displayNumber('9')}>9</button>
+          <button onClick={() => displayNumber('0')}>0</button>
+          <button onClick={() => displayNumber('c')}>Clear</button>
+          <button onClick={() => displayNumber('r')}>Recall</button>
         </div>
       </div>
 
       <div className="panel">
         <p>{operation}</p>
         <div className="numbers">
-          <button onClick={() => displayOperation("+")}>+</button>
-          <button onClick={() => displayOperation("-")}>-</button>
-          <button onClick={() => displayOperation("*")}>*</button>
-          <button onClick={() => displayOperation("÷")}>÷</button>
+          <button onClick={() => displayOperation('+')}>+</button>
+          <button onClick={() => displayOperation('-')}>-</button>
+          <button onClick={() => displayOperation('*')}>*</button>
+          <button onClick={() => displayOperation('÷')}>÷</button>
         </div>
       </div>
 
       <div className="panel">
         <p>{secondNumber}</p>
         <div className="numbers">
-          <button onClick={() => displaySecondNumber("1")}>1</button>
-          <button onClick={() => displaySecondNumber("2")}>2</button>
-          <button onClick={() => displaySecondNumber("3")}>3</button>
-          <button onClick={() => displaySecondNumber("4")}>4</button>
-          <button onClick={() => displaySecondNumber("5")}>5</button>
-          <button onClick={() => displaySecondNumber("6")}>6</button>
-          <button onClick={() => displaySecondNumber("7")}>7</button>
-          <button onClick={() => displaySecondNumber("8")}>8</button>
-          <button onClick={() => displaySecondNumber("9")}>9</button>
-          <button onClick={() => displaySecondNumber("0")}>0</button>
-          <button onClick={() => displaySecondNumber("c")}>Clear</button>
-          <button onClick={() => displaySecondNumber("r")}>Recall</button>
+          <button onClick={() => displaySecondNumber('1')}>1</button>
+          <button onClick={() => displaySecondNumber('2')}>2</button>
+          <button onClick={() => displaySecondNumber('3')}>3</button>
+          <button onClick={() => displaySecondNumber('4')}>4</button>
+          <button onClick={() => displaySecondNumber('5')}>5</button>
+          <button onClick={() => displaySecondNumber('6')}>6</button>
+          <button onClick={() => displaySecondNumber('7')}>7</button>
+          <button onClick={() => displaySecondNumber('8')}>8</button>
+          <button onClick={() => displaySecondNumber('9')}>9</button>
+          <button onClick={() => displaySecondNumber('0')}>0</button>
+          <button onClick={() => displaySecondNumber('c')}>Clear</button>
+          <button onClick={() => displaySecondNumber('r')}>Recall</button>
         </div>
       </div>
       <div className="panel answer">
         <p>{totalNumber}</p>
         <div>
           <button onClick={() => smartMaths()}>=</button>
-          <button onClick={() => setStoredNumber(totalNumber)}>Store</button>
+          <button
+            className="store-btn"
+            onClick={() => setStoredNumber(totalNumber)}
+          >
+            Store
+          </button>
         </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
